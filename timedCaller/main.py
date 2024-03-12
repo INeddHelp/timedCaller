0b4fa94a272f8e84e4c967b5375cb787f015240b
Michael Barati <101454502+INeddHelp@users.noreply.github.com>
2024-03-12 09:20:30 +0100

    Update main.py

--- timedCaller/main.py
+++ timedCaller/main.py
@@ -3,23 +3,23 @@ import time
 
 def timedCaller(seconds, interval, *args):
 
+    func_array = []
+    
     if not isinstance(seconds, int) or not isinstance(interval, int):
         raise TypeError("Seconds and interval must be integers")
 
     for func in args:
         if not callable(func):
             raise TypeError("All arguments must be callable (functions)")
+        else:
+            func_array = func_array.append(func)
 
     timer = time.time()
 
     while time.time() - timer < seconds:
 
-        if interval != 0:
-            for func in args:
-                func()
-                time.sleep(interval)
-        else:
-            for func in args:
-                func()
+        for func in args:
+            func()
+            time.sleep(interval)
 
-    print(f"Timer with {seconds} seconds has finished.")
+    print(f"The functions {func_array} have been called for {seconds} seconds with an interval of {interval} seconds")
